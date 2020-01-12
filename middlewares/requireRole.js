b1647530ae8f21966a62dedfcc520dc3efabd65d
dillon <dillonstreator@gmail.com>
2020-01-12 09:32:56 -0800

    clarify requireRole variables and use lodash get for safe object traversal

--- middlewares/requireRole.js
+++ middlewares/requireRole.js
@@ -1,8 +1,11 @@
 const logger = rootRequire("utils/logger");
+const _get = require("lodash.get");
 
-module.exports = (...roles) => {
+module.exports = (...permittedRoles) => {
    return (req, res, next) => {
-      const actionPermitted = roles.includes(req.user.role);
+      const myRole = _get(req, ["user", "role"]);
+
+      const actionPermitted = permittedRoles.includes(myRole);
       if (!actionPermitted) {
          logger.info(
             `userId: ${req.user.id} attempted to access a role restricted route!`
