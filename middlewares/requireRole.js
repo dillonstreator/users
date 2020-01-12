59c005ea12305c30d8a1448e7a2b85312da15a1f
dillon <dillonstreator@gmail.com>
2020-01-12 11:06:01 -0800

    requireRole properly sends 401 on request without jwt

--- middlewares/requireRole.js
+++ middlewares/requireRole.js
@@ -3,12 +3,18 @@ const _get = require("lodash.get");
 
 module.exports = (...permittedRoles) => {
    return (req, res, next) => {
+      const userId = _get(req, ["user", "id"]);
+      if (!userId) {
+         logger.info(`Unauthorized user attempted to access route...`);
+         return res.sendStatus(401);
+      }
+
       const myRole = _get(req, ["user", "role"]);
 
       const actionPermitted = permittedRoles.includes(myRole);
       if (!actionPermitted) {
          logger.info(
-            `userId: ${req.user.id} attempted to access a role restricted route!`
+            `userId: ${userId} attempted to access a role restricted route!`
          );
          return res.sendStatus(403);
       }
